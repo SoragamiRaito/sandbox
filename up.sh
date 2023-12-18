106532e9c5f9c3a0301044df90e61683d4025e52
SoragamiRaito <soragamiraito@gmail.com>
2023-12-18 23:57:14 +0900

    fix: up.sh

--- up.sh
+++ up.sh
@@ -7,20 +7,20 @@ BACK="no"
 
 while getopts "i:c:v:b" opt; do
   case $opt in
-    i)
-      IMAGE_NAME="$OPTARG"
-      ;;
-    c)
-      CONTAINER_NAME="$OPTARG"
-      ;;
-# コンテナ内にworkspaceディレクトリをバインドするようにする。イメージのビルドやコンテナの起動関数も変更する。
-    v)
-      VIND="$OPTARG"
-      ;;
-    ?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
+  i)
+    IMAGE_NAME="$OPTARG"
+    ;;
+  c)
+    CONTAINER_NAME="$OPTARG"
+    ;;
+    # コンテナ内にworkspaceディレクトリをバインドするようにする。イメージのビルドやコンテナの起動関数も変更する。
+  v)
+    VIND="$OPTARG"
+    ;;
+  ?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
   esac
 done
 
@@ -35,18 +35,16 @@ if [[ "$(docker images -q "$IMAGE_NAME" 2>/dev/null)" == "" || Dockerfile -nt .d
 fi
 
 # コンテナの状態を確認し、起動または再開
-if [[ "$(docker ps -q -f name="^$CONTAINER_NAME&")" == "" ]]; then
-  if [[ "VIND" ]]; then
-  docker run -v "$VIND" -d --name "^$CONTAINER_NAME&" "$IMAGE_NAME"
-  else
+if [[ "$(docker ps -a -q -f name="$CONTAINER_NAME")" == "" ]]; then
   # コンテナが起動していない場合、新たに起動
-  docker run -d --name "^$CONTAINER_NAME&" "$IMAGE_NAME"
+  if [[ "$VIND" ]]; then
+    docker run -v "$VIND" -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
+  else
+    docker run -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
   fi
-else
-  if [[ "$(docker ps -q -f status=exited -f name="^$CONTAINER_NAME&")" != "" ]]; then
-    # コンテナが停止している場合、再開
-    docker start "$CONTAINER_NAME"
-  fi
+elif [[ "$(docker ps -q -f status=exited -f name="$CONTAINER_NAME")" != "" ]]; then
+  # コンテナが停止している場合、再開
+  docker start "$CONTAINER_NAME"
 fi
 
 # -bオプションが指定されていない場合のみコンテナに接続し、コンテナ内でcdコマンドを実行
