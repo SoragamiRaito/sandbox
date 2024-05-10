52a43262659849df0cfbb12bdf42c94250b37bae
SoragamiRaito <soragamiraito@gmail.com>
2024-05-10 02:29:53 +0000

    fix devcontainer.json & post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,8 +7,13 @@ git config --global user.name "SoragamiRaito"
 # loginshellの変更
 sudo chsh -s "$(which zsh)"
 
+# .envの読み込み追加
+cp $HOME/workspace/.devcontainer/.env $HOME/.env
+echo 'source $HOME/.env' >>~/.bashrc
+echo 'source $HOME/.env' >>~/.zshrc
+
 # starshipの導入
 echo 'eval "$(starship init bash)"' >>~/.bashrc
 echo 'eval "$(starship init zsh)"' >>~/.zshrc
-cp /home/workspace/.devcontainer/starship.toml ~/.config/starship.toml
+cp $HOME/workspace/.devcontainer/starship.toml ~/.config/starship.toml
 curl -sS https://starship.rs/install.sh | sh -s -- -y
