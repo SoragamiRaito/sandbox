a324960aa5454e48d052d96bee98a1f0d46f056f
SoragamiRaito <soragamiraito@gmail.com>
2024-05-17 12:38:26 +0900

    fix devcontainer settings

--- post-create.sh
+++ post-create.sh
@@ -8,22 +8,19 @@ git config --global user.name "SoragamiRaito"
 sudo chsh -s "$(which zsh)"
 
 # .envの読み込み追加
-cp $HOME/workspace/.devcontainer/.env $HOME/.env
+cp /share/.env $HOME/.env
 # bash用
-echo '
-# read .env
-source $HOME/.env
-
-# init tools' >>~/.bashrc
+echo 'source $HOME/.env' >>~/.bashrc
 # zsh用
-echo '
-# read .env
-source $HOME/.env
-
-# init tools' >>~/.zshrc
+echo 'source $HOME/.env' >>~/.zshrc
 
 # starshipの導入
 echo 'eval "$(starship init bash)"' >>~/.bashrc
 echo 'eval "$(starship init zsh)"' >>~/.zshrc
-cp $HOME/workspace/.devcontainer/starship.toml ~/.config/starship.toml
+cp /share/starship.toml $HOME/.config/starship.toml
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# miseの導入
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >>~/.bashrc
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >>~/.zshrc
